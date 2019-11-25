3812183e3deb1580c37472cd71461088c5c56804
Kirill Vershinin <verkirill@yandex.ru>
2019-11-25 22:14:37 +0400

    доработка Exception

--- src/test/java/ru/yandex/test/BaseTest.java
+++ src/test/java/ru/yandex/test/BaseTest.java
@@ -5,15 +5,20 @@ import org.junit.Before;
 import ru.yandex.pageObject.AppManagerPage;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class BaseTest{
     protected AppManagerPage app;
 
     @Before
-    public void SetUp() throws Exception {
+    public void SetUp() {
         Properties properties = new Properties();
-        properties.load(new FileReader(new File("testinfo.properties")));
+        try {
+            properties.load(new FileReader(new File("testinfo.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         app = new AppManagerPage(properties);
     }
 
